985721064ea2b748b1cbc0cc4b70ae727fde8744
ali <alisoltanics@gmail.com>
2019-04-19 23:17:17 +0430

    first commit

---
+++ post/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='posts',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(
+        max_length=350
+    )
+    description = models.TextField()
