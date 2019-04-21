d47f7f9d6c098f5e4afdb2b237dab04f3aea4615
ali <alisoltanics@gmail.com>
2019-04-21 09:22:13 +0430

    remove secret_key in settings

--- my_blog_django/settings.py
+++ my_blog_django/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .setting_variables import get_secret_key
-SECRET_KEY = get_secret_key()
+# from .setting_variables import get_secret_key
+# SECRET_KEY = get_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
