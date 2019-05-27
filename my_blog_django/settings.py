77dff9be836bd94ee11537c07dd72924bf2a4c3a
ali <alisoltanics@gmail.com>
2019-05-27 23:50:36 +0430

    Add ckeditor to project

--- my_blog_django/settings.py
+++ my_blog_django/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "ckeditor",
     'post'
 ]
 
@@ -121,10 +123,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, './static'), ]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 
 DOMAIN = "http://127.0.0.1:8000"
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_CONFIGS = {
+    'awesome_ckeditor': {
+        'toolbar': 'Basic',
+    },
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source']
+        ]
+    }
+}
--- post/admin.py
+++ post/admin.py
@@ -1,4 +1,21 @@
+from django import forms
 from django.contrib import admin
+
 from .models import Post
 
-admin.site.register(Post)
+from ckeditor.widgets import CKEditorWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+
+admin.site.register(Post, PostAdmin)
