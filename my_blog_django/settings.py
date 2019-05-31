d5f47dad96115e0c92953091c93b449076ca1140
ali <alisoltanics@gmail.com>
2019-05-31 21:42:52 +0430

    Add django-cors-headers to project

--- my_blog_django/settings.py
+++ my_blog_django/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "ckeditor",
-    'post'
+    'post',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -146,3 +148,5 @@ CKEDITOR_CONFIGS = {
         ]
     }
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
